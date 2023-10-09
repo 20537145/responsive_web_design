e70961307b7776568ff990a719bb8f95e47c2709
ahmed sherif <ahmedcherif.10@gmail.com>
2023-10-09 12:01:00 +0100

    responsive

---
+++ src/data.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Data() {
+  return (
+    <div className='header text-center'>
+        <div className='overlay'>
+        <div className='container'>
+            <h1>Choose Your Fate</h1>
+            <p className='piti'>Study</p>
+            <p className='piti'>Die</p>
+            </div>
+        </div>
+        
+    </div>
+  )
+}
+
+export default Data
